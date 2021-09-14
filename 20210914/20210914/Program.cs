528d9362ae66eb154359c714aeb015361de9d99f
Tóth Richárd <ricsi.2003.12.30@gmail.com>
2021-09-14 14:05:44 +0200

    orai munka

---
+++ 20210914/20210914/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _20210914
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            elsofeladat();
+            masodikfeladat();
+            harmadikfeladat();
+        }
+        static void elsofeladat() {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        static void masodikfeladat()
+        {
+            Console.WriteLine("hogy hivnak");
+            string nev = Console.ReadLine();
+            Console.WriteLine("szia ", nev); 
+        }
+        static void harmadikfeladat()
+        {
+            Console.WriteLine("irj egy szamot");
+            int szam = Console.Read();
+            Console.WriteLine("a szam ketszerese: ", szam * 2);
+        }
+        static void negyedikfeladat()
+        {
+            Console.WriteLine("irj egy szamot");
+            Console.WriteLine("irj meg egy szamot");
+            int szam1 = Console.Read();
+            int szam2 = Console.Read();
+            Console.WriteLine("a ket szam osszegge: ", szam1 + szam2);
+            Console.WriteLine("a ket szam kulonbsege: ", szam1 - szam2);
+            Console.WriteLine("a ket szam szorzata: ", szam1 * szam2);
+            Console.WriteLine("a ket szam osszegge: ", szam1 / szam2);
+            Console.WriteLine("a ket szam osszegge: ", szam1 % szam2);
+        }
+        static void otodikfeladat()
+        {
+            Console.WriteLine("negyzet A oldala");
+            Console.WriteLine("negyzet B oldala");
+            int A = Console.Read();
+            int B = Console.Read();
+            Console.WriteLine("A szam negyzete: ", A * B);
+            Console.WriteLine();
+        }
+    }
+}
